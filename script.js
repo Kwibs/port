4dbb22fa92db4fd0d690e8bfe04f724979879c2f
Kwibs <kylehelstowski@gmail.com>
2024-11-03 09:09:22 -0800

    commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight); // Create canvas that covers the whole window
     noLoop(); // Only draw once
 }
 
 function draw() {
+    // Set a gradient background
     for (let i = 0; i <= height; i++) {
         let inter = map(i, 0, height, 0, 1);
         let c = lerpColor(color('#3c8453'), color('#bcaa96'), inter);
@@ -13,5 +14,6 @@ function draw() {
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight); // Resize the canvas when the window is resized
 }
+
