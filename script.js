52dc5175d11ad4695aa510228a9c67e2e21f5f1f
Kwibs <kylehelstowski@gmail.com>
2024-12-01 11:26:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight); // Create canvas that covers the whole window
-    noLoop(); // Only draw once
-}
+// function setup() {
+//     createCanvas(windowWidth, windowHeight); // Create canvas that covers the whole window
+//     noLoop(); // Only draw once
+// }
 
-function draw() {
-    // Set a gradient background
-    for (let i = 0; i <= height; i++) {
-        let inter = map(i, 0, height, 0, 1);
-        let c = lerpColor(color('#3c8453'), color('#bcaa96'), inter);
-        stroke(c);
-        line(0, i, width, i);
-    }
-}
+// function draw() {
+//     // Set a gradient background
+//     for (let i = 0; i <= height; i++) {
+//         let inter = map(i, 0, height, 0, 1);
+//         let c = lerpColor(color('#3c8453'), color('#bcaa96'), inter);
+//         stroke(c);
+//         line(0, i, width, i);
+//     }
+// }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight); // Resize the canvas when the window is resized
-}
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight); // Resize the canvas when the window is resized
+// }
 
